3d70939ebf0d0a6339f5ed4578b6cdba5abd34a3
DarshanSoneji1 <125848114+DarshanSoneji1@users.noreply.github.com>
2023-03-10 08:44:44 +0530

    Update in Interviewer controller - With SP
    11-03-2023

--- MVCProject.Api/Controllers/InterviewerRegister/InterviewersController.cs
+++ MVCProject.Api/Controllers/InterviewerRegister/InterviewersController.cs
@@ -6,6 +6,7 @@ using NPOI.HSSF.Record;
 #region namespaces
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -21,42 +22,58 @@ namespace MVCProject.Api.Controllers.Interviewers
         {
             this.entities = new MVCProjectEntities();
         }
-
         [HttpPost]
         public ApiResponse GetAllInterviewers(PagingParams interviewerDetailParams)
         {
-            //var interviewerslist = this.entities.Interviewers.Select(d => new
+            var interviewerlist = entities.USP_ATS_InterviewerList().ToList();
+            //.AsEnumerable()
+            // .AsQueryable().OrderByField(interviewerDetailParams.OrderByColumn, interviewerDetailParams.IsAscending)
+            // .Skip((interviewerDetailParams.CurrentPageNumber - 1) * interviewerDetailParams.PageSize).Take(interviewerDetailParams.PageSize);
+            return this.Response(MessageTypes.Success, string.Empty, interviewerlist);
+        }
+        //[HttpPost]
+        //public ApiResponse GetAllInterviewers(PagingParams interviewerDetailParams)
+        //{
+        //    //var interviewerslist = this.entities.Interviewers.Select(d => new
+        //    //{
+        //    //    InterviewerId = d.InterviewerId,
+        //    //    InterviewerName = d.InterviewerName,
+        //    //    InterviewerEmail = d.InterviewerEmail,
+        //    //    InterviewerPhone = d.InterviewerPhone
+        //    //}).ToList();
+        //    //return this.Response(MessageTypes.Success, string.Empty, interviewerslist);
+        //    var interviewerslist = (from d in this.entities.ATS_Interviewer.AsEnumerable()
+        //                        let TotalRecords = this.entities.ATS_Interviewer.AsEnumerable().Count()
+        //                        select new
+        //                        {
+        //                            InterviewerId = d.InterviewerId,
+        //                            InterviewerName = d.InterviewerName,
+        //                            InterviewerEmail = d.InterviewerEmail,
+        //                            InterviewerPhone = d.InterviewerPhone,
+        //                            TotalRecords
+        //                        }).AsQueryable().Skip((interviewerDetailParams.CurrentPageNumber - 1) * interviewerDetailParams.PageSize).Take(interviewerDetailParams.PageSize);
+        //    return this.Response(MessageTypes.Success, string.Empty, interviewerslist);
+        //}
+
+        [HttpGet]
+        public ApiResponse GetInterviewerById(int InterviewerId)
+        {
+            var InterviewerDetail = this.entities.USP_ATS_InterviewerbyId(InterviewerId).Select(d => new
+            {
+                InterviewerId = d.InterviewerId,
+                InterviewerName = d.InterviewerName,
+                InterviewerEmail = d.InterviewerEmail,
+                InterviewerPhone = d.InterviewerPhone,
+                Is_Active = d.Is_Active
+            }).SingleOrDefault();
+            //.ATS_Interviewer.Where(x => x.InterviewerId == InterviewerId)
+            //.Select(d => new
             //{
             //    InterviewerId = d.InterviewerId,
             //    InterviewerName = d.InterviewerName,
             //    InterviewerEmail = d.InterviewerEmail,
             //    InterviewerPhone = d.InterviewerPhone
-            //}).ToList();
-            //return this.Response(MessageTypes.Success, string.Empty, interviewerslist);
-            var interviewerslist = (from d in this.entities.ATS_Interviewer.AsEnumerable()
-                                let TotalRecords = this.entities.ATS_Interviewer.AsEnumerable().Count()
-                                select new
-                                {
-                                    InterviewerId = d.InterviewerId,
-                                    InterviewerName = d.InterviewerName,
-                                    InterviewerEmail = d.InterviewerEmail,
-                                    InterviewerPhone = d.InterviewerPhone,
-                                    TotalRecords
-                                }).AsQueryable().Skip((interviewerDetailParams.CurrentPageNumber - 1) * interviewerDetailParams.PageSize).Take(interviewerDetailParams.PageSize);
-            return this.Response(MessageTypes.Success, string.Empty, interviewerslist);
-        }
-
-        [HttpGet]
-        public ApiResponse GetInterviewerById(int InterviewerId)
-        {
-            var InterviewerDetail = this.entities.ATS_Interviewer.Where(x => x.InterviewerId == InterviewerId)
-                .Select(d => new
-                {
-                    InterviewerId = d.InterviewerId,
-                    InterviewerName = d.InterviewerName,
-                    InterviewerEmail = d.InterviewerEmail,
-                    InterviewerPhone = d.InterviewerPhone
-                }).SingleOrDefault();
+            //}).SingleOrDefault();
             if (InterviewerDetail != null)
             {
                 return this.Response(Utilities.MessageTypes.Success, string.Empty, InterviewerDetail);
@@ -71,8 +88,11 @@ namespace MVCProject.Api.Controllers.Interviewers
         public ApiResponse Register([FromBody] ATS_Interviewer data)
         {
             var InterviewerData = this.entities.ATS_Interviewer.FirstOrDefault(x => x.InterviewerId == data.InterviewerId);
+
             if (InterviewerData == null)
             {
+
+                data.EntryDate = DateTime.Now;
                 entities.ATS_Interviewer.AddObject(data);
                 if (!(this.entities.SaveChanges() > 0))
                 {
@@ -86,6 +106,8 @@ namespace MVCProject.Api.Controllers.Interviewers
                 InterviewerData.InterviewerName = data.InterviewerName;
                 InterviewerData.InterviewerEmail = data.InterviewerEmail;
                 InterviewerData.InterviewerPhone = data.InterviewerPhone;
+                InterviewerData.Is_Active = data.Is_Active;
+                InterviewerData.UpdateDate = DateTime.Now;
 
                 this.entities.ATS_Interviewer.ApplyCurrentValues(InterviewerData);
                 if (!(this.entities.SaveChanges() > 0))
